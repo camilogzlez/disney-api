65b5111dd1dff93a83c1be30e177d027e80b2c53
camilo gonzalez <camilogzlez@gmail.com>
2021-04-29 10:19:53 -0300

    Update endpoint done

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::CharactersController < Api::V1::BaseController
+  acts_as_token_authentication_handler_for User, except: [ :index, :show ]
   
-  before_action :set_character, only: [ :show ]
+  before_action :set_character, only: [:show, :update]
   
   def index
     @characters = policy_scope(Character)
@@ -9,10 +10,27 @@ class Api::V1::CharactersController < Api::V1::BaseController
   def show
   end
 
+  def update
+     if @character.update(character_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
   private
+
+  def character_params
+    params.require(:character).permit(:name, :age, :weight, :history, :photo)
+  end
 
   def set_character
     @character = Character.find(params[:id])
     authorize @character
+  end
+
+   def render_error
+    render json: { errors: @character.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,6 +1,6 @@
 class Character < ApplicationRecord
   belongs_to :user
-  has_many :participations
+  has_many :participations, dependent: :destroy
   has_many :pieces, through: :participations
   has_one_attached :photo
 end
